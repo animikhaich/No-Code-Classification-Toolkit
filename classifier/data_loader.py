f132b2e4bc591dd00dc1280750225a661f119e0a
Animikh Aich <animikhaich@gmail.com>
2021-03-30 07:14:37 +0530

    Updated Gitignore, Added Data Loader Skeleton

---
+++ classifier/data_loader.py
@@ -0,0 +1,25 @@
+import os
+import tensorflow as tf
+
+
+class ImageClassificationDataLoader:
+    def __init__(self) -> None:
+        pass
+
+    def __get_labels(self) -> None:
+        pass
+
+    def __load_image(self) -> None:
+        pass
+
+    def __augment_batch(self) -> None:
+        pass
+
+    def __load_config(self) -> None:
+        pass
+    
+    def get_num_steps(self) -> None:
+        pass
+
+    def dataset_generator(self) -> None:
+        pass
